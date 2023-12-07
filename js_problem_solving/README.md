f3ea77e6e2cfff90f34fe737c5648193387ad72d
Md Sayeful Islam <sayefulislam367@gmail.com>
2023-12-07 23:16:16 +0600

    problem27 solve

--- js_problem_solving/README.md
+++ js_problem_solving/README.md
@@ -1,5 +1,13 @@
 ## All problems are solved in the JavaScript programming language.
 
+27. problem27
+- Given a square grid of characters in the range ascii[a-z], rearrange elements of each row alphabetically, ascending. Determine if the columns are also in ascending alphabetical order, top to bottom. Return YES if they are or NO if they are not.
+grid = ['abc', 'ade', 'efg'];
+- The rows are already in alphabetical order. The columns a a e, b d f and c e g are also in alphabetical order, so the answer would be YES. Only elements within the same row can be rearranged. They cannot be moved to a different row.
+```bash
+input ['abc', 'ade', 'efg']
+output YES
+```
 26. problem26
 - Declare a 2-dimensional array, arr, of n empty arrays. All arrays are zero indexed.
 - Declare an integer, lastAnswer, and initialize it to 0.
---
+++ problem27.js
@@ -0,0 +1,33 @@
+// - Given a square grid of characters in the range ascii[a-z], rearrange elements of each row alphabetically, ascending. Determine if the columns are also in ascending alphabetical order, top to bottom. Return YES if they are or NO if they are not.
+// grid = ['abc', 'ade', 'efg'];
+// - The rows are already in alphabetical order. The columns a a e, b d f and c e g are also in alphabetical order, so the answer would be YES. Only elements within the same row can be rearranged. They cannot be moved to a different row.
+// output YES
+
+const gridArr = ['abc', 'ade', 'efg'];
+
+function gridChallenge(grid) {
+  let Grid = grid.map(item => {
+    let temp = item.split('').sort();
+    return temp;
+  });
+  
+  let column = [];
+  let gridLength = Grid.length;
+  let gridLength1 = Grid[0].length;
+  for(let i = 0; i<gridLength1; i++){
+    let temp = '';
+    for(let j = 0; j<gridLength; j++){
+      temp += Grid[j][i];
+    };
+    column.push(temp);
+  };
+  
+  let filterColumn = column.filter(item => {
+    let temp = item.split('').sort().join('');
+    return item === temp;
+  });
+
+  return filterColumn.length === column.length ? "YES" : "NO";
+}
+
+console.log(gridChallenge(gridArr));
